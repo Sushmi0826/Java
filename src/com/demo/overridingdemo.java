0ad67bc5a055ed4cbee2ad764d38089e7576a4ec
Users <sushmiguptha@gmail.com>
2023-03-07 19:13:37 +0530

    Adding files

---
+++ src/com/demo/overridingdemo.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class overridingdemo {
+    public void eat() {
+        System.out.println("Eat in animal");
+    }
+    public void sleep() {
+        System.out.println("Animals need rest");
+    }
+
+}
