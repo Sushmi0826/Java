2a0b52dfe88777f9b06b04153f8498a252b41623
Users <sushmiguptha@gmail.com>
2023-03-07 19:05:40 +0530

    Adding files

---
+++ src/com/demo/Decisiondemo.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class Decisiondemo {
+    public static void main(String[] args){
+        int x = 10 ; int y = 20;
+        if (x<y)
+            System.out.println("False");
+        else
+            System.out.println("True");
+        System.out.println("outside the if condition");
+    }
+}
